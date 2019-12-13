5ad2d48eb5eaef85d8f90ecec3a21689e4771207
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-12-13 13:17:16 +0100

    chore: set base font-size

--- src/components/app/common.ts
+++ src/components/app/common.ts
@@ -1,12 +1,17 @@
 import { css } from 'styled-components';
 
 export default css`
+  html {
+    font-size: 62.5%;
+  }
+
   html,
   body {
     height: 100%;
   }
 
   body {
+    font-size: 1.6rem;
     overflow-x: hidden;
     overflow-y: scroll;
   }
