3b596361f34e5b959a1faff5ae0fe5e5a439f9e2
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-12-17 08:43:12 +0100

    chore: update domain types from specc

--- src/types/domain.d.ts
+++ src/types/domain.d.ts
@@ -2,9 +2,27 @@ import { RecordStatus } from './enums';
 
 export interface RecordInterface {
   id: string;
-  title: string;
-  dataProcessorContactDetails: ContactDetailsInterface;
   status: RecordStatus;
+  organizationId: string;
+  purpose: string;
+  dataSubjectCategories: string[];
+  personalDataCategories: string[];
+  recipientCategories: string;
+  personalDataSubjects: string;
+  plannedDeletion: string;
+  highPrivacyRisk: boolean;
+  articleSixBasis: ArticleSixBasis[];
+  otherArticles: OtherArticles[];
+  businessArea: string[];
+  securityMeasures: string;
+  privacyProcessingSystems: string;
+  dataProcessorContactDetails: ContactDetailsInterface;
+  commonDataControllerContact: CommonDataControllerContact;
+  dataTransfers: DataTransfers;
+  title: string;
+  relatedDatasets: string[];
+  dataProtectionImpactAssessment: DataProtectionImpactAssessment;
+  dataProcessingAgreement: DataProcessingAgreement;
 }
 
 export interface ContactDetailsInterface {
@@ -18,4 +36,42 @@ export interface RepresentativesInterface {
   dataControllerRepresentative: ContactDetailsInterface;
   dataControllerRepresentativeInEU: ContactDetailsInterface;
   dataProtectionOfficer: ContactDetailsInterface;
+}
+
+export interface ArticleSixBasis {
+  legality: string;
+  referenceUrl: string;
+}
+
+export interface Article {
+  checked: boolean;
+  referenceUrl: string;
+}
+
+export interface OtherArticles {
+  articleNine: Article;
+  articleTen: Article;
+}
+
+export interface CommonDataControllerContact {
+  companies: string;
+  distributionOfResponsibilities: string;
+  contactPoints: ContactDetailsInterface[];
+}
+
+export interface DataTransfers {
+  transfered: boolean;
+  thirdCountryRecipients: string;
+  guarantees: string;
+  internationalOrganizations: string;
+}
+
+export interface DataProtectionImpactAssessment {
+  conducted: boolean;
+  assessmentReportUrl: string;
+}
+
+export interface DataProcessingAgreement {
+  dataProcessorName: string;
+  agreementUrl: string;
 }
