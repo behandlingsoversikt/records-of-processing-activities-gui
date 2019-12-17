9b1c0197ecd234c390bcb7302526d7df64758987
Alexander Zakharov <alexander.zakharov@capgemini.com>
2019-12-17 10:26:25 +0100

    feat: add logout functionality

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,73 +1,37 @@
 import React, { memo } from 'react';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
-// import { withAuth } from '../../providers/auth';
-// import { AuthServiceInteface, User } from '../../services/auth';
+import { withAuth } from '../../providers/auth';
+import { AuthServiceInteface, User } from '../../services/auth';
 
 import SC from './styled';
-// import Common from '../common/styled';
 
-const Header = (): JSX.Element => {
+interface Props {
+  authService: AuthServiceInteface;
+}
+
+const Header = ({ authService }: Props): JSX.Element => {
+  const user: User | null = authService.getUser();
+  const logOutAndRedirect = () => authService.logOut(`${location.origin}/auth`);
+
   return (
     <SC.Header>
       <SC.Container>
         <SC.LogoLink href='/'>
           <SC.Logo />
         </SC.LogoLink>
-        <SC.UserAvatar>
-          <AccountCircleIcon />
-          <SC.UserName>JOHN DIE</SC.UserName>
-          <SC.LogoutButton variant='outlined'>Log out</SC.LogoutButton>
-        </SC.UserAvatar>
+        {user && (
+          <SC.UserAvatar>
+            <AccountCircleIcon />
+            <SC.UserName>{user.profile.name}</SC.UserName>
+            <SC.LogoutButton variant='outlined' onClick={logOutAndRedirect}>
+              Log out
+            </SC.LogoutButton>
+          </SC.UserAvatar>
+        )}
       </SC.Container>
     </SC.Header>
   );
 };
 
-export default memo(Header);
-
-// interface Props {
-//   authService: AuthServiceInteface;
-// }
-
-// class Header extends PureComponent<Props> {
-//   constructor(props: Props) {
-//     super(props);
-
-//     this.logOutAndRedirect = this.logOutAndRedirect.bind(this);
-//   }
-
-//   // private getUserName(): string | null {
-//   //   const { authService } = this.props;
-//   //   const user: User | null = authService.getUser();
-//   //   return user ? user.profile.name : null;
-//   // }
-
-//   // private async logOutAndRedirect(): Promise<void> {
-//   //   const { authService } = this.props;
-//   //   await authService.logOut(`${location.origin}/auth`);
-//   // }
-
-//   public render(): JSX.Element {
-//     const userName: string | null = this.getUserName();
-//     return (
-//       <SC.Header>
-//         <SC.Logo />
-//         {userName && (
-// <SC.UserAvatar>
-//   <AccountCircleIcon />
-//   <SC.UserName>{userName}</SC.UserName>
-//   <SC.LogoutButton
-//     variant='outlined'
-//     onClick={this.logOutAndRedirect}
-//   >
-//     Log out
-//   </SC.LogoutButton>
-// </SC.UserAvatar>
-//         )}
-//       </SC.Header>
-//     );
-//   }
-// }
-
-// export default withAuth(Header);
+export default memo(withAuth(Header));
