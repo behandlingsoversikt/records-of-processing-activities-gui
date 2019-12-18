65eb33bf0507be58fa33307f75e42a89424d6417
Alexander Zakharov <alexander.zakharov@capgemini.com>
2019-12-18 11:49:39 +0100

    feat: #2761 connect first section of form to formik

--- src/components/record-form/index.tsx
+++ src/components/record-form/index.tsx
@@ -1,51 +1,181 @@
-import React, { memo } from 'react';
-import { withFormik } from 'formik';
+import React, { memo, Fragment } from 'react';
+import { FormikProps, withFormik, FieldArray } from 'formik';
 
 import TextField from '../field-text';
 import TextAreaField from '../field-text-area';
 
 import SC from './styled';
 
-import validationSchema from './validation-schema';
+import AddIcon from '../../images/icon-add.svg';
+import RemoveIcon from '../../images/icon-remove.svg';
 
-const RecordForm = () => (
+import validationSchema from './validation-schema';
+import { Record } from '../../types/domain';
+
+interface Props extends FormikProps<Record> {
+  record?: Partial<Record>;
+}
+
+const RecordForm = ({ values, handleChange }: Props) => (
   <SC.RecordForm>
     <SC.RecordFormSection title='Behandlingsansvar og databehandler'>
       <SC.Fieldset
         title='Daglig behandlingsanvar'
         subtitle='Den korte hjelpeteksten'
       >
-        <TextField name='name' labelText='Kontaktpunkt' />
+        <TextField
+          name='dataProcessorContactDetails.name'
+          value={values.dataProcessorContactDetails.name}
+          labelText='Kontaktpunkt'
+          onChange={handleChange}
+        />
         <SC.InlineFields>
-          <TextField name='email' labelText='E-post' />
-          <TextField name='phone' labelText='Telefon' />
+          <TextField
+            name='dataProcessorContactDetails.email'
+            value={values.dataProcessorContactDetails.email}
+            labelText='E-post'
+            onChange={handleChange}
+          />
+          <TextField
+            name='dataProcessorContactDetails.phone'
+            value={values.dataProcessorContactDetails.phone}
+            labelText='Telefon'
+            onChange={handleChange}
+          />
         </SC.InlineFields>
       </SC.Fieldset>
       <SC.Fieldset
         title='Databehandlere og databehandleravtaler'
         subtitle='Den korte hjelpeteksten'
       >
-        <TextField name='name' labelText='Navn på databehandler' />
-        <TextField name='email' labelText='Lenke til databehandleravtale' />
+        <FieldArray
+          name='dataProcessingAgreements'
+          render={arrayHelpers => (
+            <>
+              {values.dataProcessingAgreements.map(
+                ({ dataProcessorName, agreementUrl }, index) => (
+                  <Fragment key={`dataProcessingAgreements-${index}`}>
+                    <TextField
+                      name={`dataProcessingAgreements[${index}].dataProcessorName`}
+                      value={dataProcessorName}
+                      labelText='Navn på databehandler'
+                      onChange={handleChange}
+                    />
+                    <TextField
+                      name={`dataProcessingAgreements[${index}].agreementUrl`}
+                      value={agreementUrl}
+                      labelText='Lenke til databehandleravtale'
+                      onChange={handleChange}
+                    />
+                    {values.dataProcessingAgreements.length > 1 && (
+                      <SC.RemoveButton
+                        type='button'
+                        onClick={() => arrayHelpers.remove(index)}
+                      >
+                        <RemoveIcon />
+                        Slett databehandler
+                      </SC.RemoveButton>
+                    )}
+                  </Fragment>
+                )
+              )}
+              <SC.AddButton
+                type='button'
+                addMargin={values.dataProcessingAgreements.length === 1}
+                onClick={() =>
+                  arrayHelpers.push({
+                    dataProcessorName: '',
+                    agreementUrl: ''
+                  })
+                }
+              >
+                <AddIcon />
+                Legg til ny databehandler
+              </SC.AddButton>
+            </>
+          )}
+        />
       </SC.Fieldset>
       <SC.Fieldset
         title='Felles databehandlingsansvar'
         subtitle='Den korte hjelpeteksten'
       >
         <TextField
-          name='name'
+          name='commonDataControllerContact.companies'
+          value={values.commonDataControllerContact.companies}
           labelText='Virksomheter som har felles databehandlingsansvar'
+          onChange={handleChange}
         />
-        <TextAreaField name='email' labelText='Ansvarsfordeling' />
         <TextField
-          name='name'
-          labelText='Kontaktpunkt'
-          placeholder='Virksomhet, avdeling, rolle eller person'
+          name='commonDataControllerContact.distributionOfResponsibilities'
+          value={
+            values.commonDataControllerContact.distributionOfResponsibilities
+          }
+          labelText='Ansvarsfordeling'
+          onChange={handleChange}
         />
-        <SC.InlineFields>
-          <TextField name='email' labelText='E-post' />
-          <TextField name='phone' labelText='Telefon' />
-        </SC.InlineFields>
+        <FieldArray
+          name='commonDataControllerContact.contactPoints'
+          render={arrayHelpers => (
+            <>
+              {(values.commonDataControllerContact.contactPoints || []).map(
+                ({ name, email, phone }, index) => (
+                  <Fragment
+                    key={`commonDataControllerContact.contactPoints-${index}`}
+                  >
+                    <TextField
+                      name={`commonDataControllerContact.contactPoints[${index}].name`}
+                      value={name}
+                      labelText='Kontaktpunkt'
+                      onChange={handleChange}
+                    />
+                    <SC.InlineFields>
+                      <TextField
+                        name={`commonDataControllerContact.contactPoints[${index}].email`}
+                        value={email}
+                        labelText='E-post'
+                        onChange={handleChange}
+                      />
+                      <TextField
+                        name={`commonDataControllerContact.contactPoints[${index}].phone`}
+                        value={phone}
+                        labelText='Telefon'
+                        onChange={handleChange}
+                      />
+                    </SC.InlineFields>
+                    {(values.commonDataControllerContact.contactPoints || [])
+                      .length > 1 && (
+                      <SC.RemoveButton
+                        type='button'
+                        onClick={() => arrayHelpers.remove(index)}
+                      >
+                        <RemoveIcon />
+                        Slett kontaktpunkt
+                      </SC.RemoveButton>
+                    )}
+                  </Fragment>
+                )
+              )}
+              <SC.AddButton
+                type='button'
+                addMargin={
+                  (values.commonDataControllerContact.contactPoints || [])
+                    .length === 1
+                }
+                onClick={() =>
+                  arrayHelpers.push({
+                    name: '',
+                    email: '',
+                    phone: ''
+                  })
+                }
+              >
+                <AddIcon />
+                Legg til nytt kontaktpunkt
+              </SC.AddButton>
+            </>
+          )}
+        />
       </SC.Fieldset>
     </SC.RecordFormSection>
     <SC.RecordFormSection required title='Behandlingsaktiviteter'>
@@ -163,9 +293,34 @@ const RecordForm = () => (
 
 export default memo(
   withFormik({
-    mapPropsToValues: () => ({}),
+    mapPropsToValues: ({
+      record: {
+        dataProcessorContactDetails: { name = '', email = '', phone = '' } = {},
+        dataProcessingAgreements = [],
+        commonDataControllerContact: {
+          companies = '',
+          distributionOfResponsibilities = '',
+          contactPoints = []
+        } = {}
+      } = {}
+    }: Props) => ({
+      dataProcessorContactDetails: {
+        name,
+        email,
+        phone
+      },
+      dataProcessingAgreements: [
+        ...dataProcessingAgreements,
+        { dataProcessorName: '', agreementUrl: '' }
+      ],
+      commonDataControllerContact: {
+        companies,
+        distributionOfResponsibilities,
+        contactPoints: [...contactPoints, { name: '', email: '', phone: '' }]
+      }
+    }),
     handleSubmit: () => {},
     validationSchema,
     displayName: 'RecordForm'
-  })(RecordForm)
+  })(RecordForm) as any
 );
