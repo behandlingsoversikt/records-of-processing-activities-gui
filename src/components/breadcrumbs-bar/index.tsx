bbd3ad22a1482d3198fe418fe7c580ffd0370034
Alexander Zakharov <alexander.zakharov@capgemini.com>
2019-12-18 11:44:06 +0100

    refactor: remove unnecessary code and fix font size

--- src/components/breadcrumbs-bar/index.tsx
+++ src/components/breadcrumbs-bar/index.tsx
@@ -1,23 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { memo } from 'react';
-import { Breadcrumbs, Link, Typography } from '@material-ui/core';
+import { Breadcrumbs, Link } from '@material-ui/core';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
-import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 import SC from './styled';
 
-interface Props extends RouteComponentProps {}
-
-const BreadcrumbsBar = (_props: Props): JSX.Element => (
+const BreadcrumbsBar = (): JSX.Element => (
   <SC.BreadcrumbsBar>
     <Breadcrumbs
       separator={<NavigateNextIcon fontSize='small' />}
       aria-label='breadcrumb'
     >
       <Link href='/'>Alle kataloger</Link>
-
-      <Typography>Behandlingsaktiviteter</Typography>
+      <span>Behandlingsaktiviteter</span>
     </Breadcrumbs>
   </SC.BreadcrumbsBar>
 );
