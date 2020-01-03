1b033c81ec7dc279b1e729d9ff96402bf93577dd
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-01-03 15:55:19 +0100

    feat(record-row): add navigation on click

--- src/components/record-row/index.tsx
+++ src/components/record-row/index.tsx
@@ -1,23 +1,32 @@
 import React, { memo } from 'react';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+
+import SC from './styled';
 
 import { Record } from '../../types/domain';
 
-interface Props {
+interface Props extends RouteComponentProps {
   record: Record;
 }
 
 const RecordRow = ({
   record: {
+    id,
+    organizationId,
     title,
     dataProcessorContactDetails: { name },
     status
-  }
-}: Props) => (
-  <tr>
-    <td>{title}</td>
-    <td>{name}</td>
-    <td>{status}</td>
-  </tr>
-);
+  },
+  history: { push }
+}: Props) => {
+  const navigateToRecord = () => push(`/${organizationId}/records/${id}`);
+  return (
+    <SC.RecordRow onClick={navigateToRecord}>
+      <td>{title}</td>
+      <td>{name}</td>
+      <td>{status}</td>
+    </SC.RecordRow>
+  );
+};
 
-export default memo(RecordRow);
+export default memo(withRouter(RecordRow));
---
+++ src/components/record-row/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const RecordRow = styled.tr`
+  cursor: pointer;
+`;
+
+export default { RecordRow };
