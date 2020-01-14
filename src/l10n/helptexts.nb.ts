ecd296fd0ef7ae41157102867f24461a0a47f2eb
Øyvind Isaksen <oyvindisaks1@gmail.com>
2020-01-14 12:28:01 +0100

    feat: add helptexts to representatives forms

---
+++ src/l10n/helptexts.nb.ts
@@ -0,0 +1,12 @@
+export const helptextsNb = {
+  dataControllerRepresentativeAbstract:
+    'Behandlingsansvarliges representant er ofte virksomhetens øverste leder.',
+  dataControllerRepresentativeDescription: `En “behandlingsansvarlig” er en fysisk eller juridisk person, en offentlig myndighet, en institusjon eller ethvert annet organ som alene eller sammen med andre bestemmer formålet med behandlingen av personopplysninger og hvilke midler som skal benyttes; når formålet med og midlene for behandlingen er fastsatt i unionsretten eller i medlemsstatenes nasjonale rett, kan den behandlingsansvarlige, eller de særlige kriteriene for utpeking av vedkommende, fastsettes i unionsretten eller i medlemsstatenes nasjonale rett, jfr. Artikkel 4 nr 7.  
+
+Dersom den behandlingsansvarlige er etablert utenfor EØS-området, skal den utpeke en representant.  
+En “representant” en fysisk eller juridisk person som er etablert i Unionen, som den behandlingsansvarlige eller databehandleren har utpekt skriftlig i henhold til artikkel 27, og som representerer den behandlingsansvarlige eller databehandleren med hensyn til de forpliktelser de har i henhold til personvernforordningen.
+
+Les mer på [Datatilsynets nettsider](https://datatilsynet.no)`,
+  dataControllerRepresentativeInEUAbstract: `Et personvernombud skal gi råd om hvordan den behandlingsansvarlige best mulig kan ivareta personverninteressene. `,
+  dataControllerRepresentativeInEUDescription: `Noen virksomheter har plikt til å ha ombud, mens andre kan ha det dersom de ønsker, se artikkel 37 flg eller les mer på [Datatilsynets nettsider](https://datatilsynet.no)`
+};
--- src/lib/localization.ts
+++ src/lib/localization.ts
@@ -1,12 +1,13 @@
 import LocalizedStrings, { LocalizedStringsMethods } from 'react-localization';
 import nb from '../l10n/nb';
+import { helptextsNb } from '../l10n/helptexts.nb';
 
 interface LocaleStrings extends LocalizedStringsMethods {
   [key: string]: any;
 }
 
 export const localization: LocaleStrings = new LocalizedStrings({
-  nb: { ...nb }
+  nb: { ...nb, ...helptextsNb }
 });
 
 localization.setLanguage('nb');
