83b5b817e563e4e39945f571af933e882901f4c9
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-01-24 12:44:01 +0100

    feat: add validation to mandatory fields

--- src/components/record-form/validation-schema.ts
+++ src/components/record-form/validation-schema.ts
@@ -1,3 +1,34 @@
 import * as Yup from 'yup';
 
-export default Yup.object().shape({});
+import { RecordStatus } from '../../types/enums';
+
+export default Yup.object().shape({
+  status: Yup.string().oneOf([RecordStatus.DRAFT, RecordStatus.APPROVED]),
+  title: Yup.string()
+    .ensure()
+    .required('Feltet må fylles ut'),
+  purpose: Yup.string()
+    .ensure()
+    .required('Feltet må fylles ut'),
+  dataSubjectCategories: Yup.array()
+    .of(Yup.string().ensure())
+    .min(1, 'Feltet må fylles ut')
+    .required('Feltet må fylles ut'),
+  personalDataCategories: Yup.array()
+    .of(Yup.string().ensure())
+    .min(1, 'Feltet må fylles ut')
+    .required('Feltet må fylles ut'),
+  securityMeasures: Yup.string()
+    .ensure()
+    .required('Feltet må fylles ut'),
+  plannedDeletion: Yup.string()
+    .ensure()
+    .required('Feltet må fylles ut'),
+  recipientCategories: Yup.array()
+    .of(Yup.string().ensure())
+    .min(1, 'Feltet må fylles ut')
+    .required('Feltet må fylles ut'),
+  dataTransfers: Yup.object().shape({
+    transferred: Yup.boolean().required('Feltet må fylles ut')
+  })
+});
