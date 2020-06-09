2d6c37c4c63ceafbb72c512db392bfc2d0830eaa
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-06-09 16:49:14 +0200

    chore(project): update dependencies and fix breaking changes

--- src/components/record-form/validation-schema.ts
+++ src/components/record-form/validation-schema.ts
@@ -4,12 +4,8 @@ import { RecordStatus } from '../../types/enums';
 
 export default Yup.object().shape({
   status: Yup.string().oneOf([RecordStatus.DRAFT, RecordStatus.APPROVED]),
-  title: Yup.string()
-    .ensure()
-    .required('Feltet må fylles ut'),
-  purpose: Yup.string()
-    .ensure()
-    .required('Feltet må fylles ut'),
+  title: Yup.string().ensure().required('Feltet må fylles ut'),
+  purpose: Yup.string().ensure().required('Feltet må fylles ut'),
   dataSubjectCategories: Yup.array()
     .of(Yup.string().ensure())
     .min(1, 'Feltet må fylles ut')
@@ -18,12 +14,8 @@ export default Yup.object().shape({
     .of(Yup.string().ensure())
     .min(1, 'Feltet må fylles ut')
     .required('Feltet må fylles ut'),
-  securityMeasures: Yup.string()
-    .ensure()
-    .required('Feltet må fylles ut'),
-  plannedDeletion: Yup.string()
-    .ensure()
-    .required('Feltet må fylles ut'),
+  securityMeasures: Yup.string().ensure().required('Feltet må fylles ut'),
+  plannedDeletion: Yup.string().ensure().required('Feltet må fylles ut'),
   recipientCategories: Yup.array()
     .of(Yup.string().ensure())
     .min(1, 'Feltet må fylles ut')
@@ -34,17 +26,13 @@ export default Yup.object().shape({
       .ensure()
       .when('transferred', {
         is: true,
-        then: Yup.string()
-          .ensure()
-          .required('Feltet må fylles ut')
+        then: Yup.string().ensure().required('Feltet må fylles ut')
       }),
     guarantees: Yup.string()
       .ensure()
       .when('transferred', {
         is: true,
-        then: Yup.string()
-          .ensure()
-          .required('Feltet må fylles ut')
+        then: Yup.string().ensure().required('Feltet må fylles ut')
       })
   })
 });
--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,15 +1,14 @@
-import path from 'path';
+import { resolve } from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
-import { BaseHrefWebpackPlugin } from 'base-href-webpack-plugin';
 
 export default {
   entry: {
     main: './src/entrypoints/main/index.tsx'
   },
   output: {
-    path: path.resolve(__dirname, '../dist'),
+    path: resolve(__dirname, '../dist'),
     publicPath: '/'
   },
   resolve: {
@@ -43,7 +42,7 @@ export default {
           {
             loader: 'babel-loader',
             options: {
-              configFile: path.resolve(__dirname, '../.babelrc')
+              configFile: resolve(__dirname, '../.babelrc')
             }
           },
           'ts-loader'
@@ -92,38 +91,15 @@ export default {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new (class ChunksFromEntryPlugin {
-      apply(compiler) {
-        compiler.hooks.emit.tap('ChunksFromEntryPlugin', compilation => {
-          compilation.hooks.htmlWebpackPluginAlterChunks.tap(
-            'ChunksFromEntryPlugin',
-            (_, { plugin }) =>
-              compilation.entrypoints
-                .get(plugin.options.entry)
-                .chunks.map(chunk => ({
-                  names: chunk.name ? [chunk.name] : [],
-                  files: chunk.files.slice(),
-                  size: chunk.modulesSize(),
-                  hash: chunk.hash
-                }))
-          );
-        });
-      }
-    })(),
     new HtmlWebpackPlugin({
-      entry: 'main',
       template: './src/entrypoints/main/index.html',
       filename: 'index.html',
-      favicon: './src/images/favicon.ico'
+      favicon: './src/images/favicon.ico',
+      base: '/',
+      chunks: ['main']
     }),
-    new CopyWebpackPlugin(
-      [{ from: './src/lib/auth/silent-check-sso.html', to: './' }],
-      {
-        copyUnmodified: true
-      }
-    ),
-    new BaseHrefWebpackPlugin({
-      baseHref: '/'
+    new CopyWebpackPlugin({
+      patterns: [{ from: './src/lib/auth/silent-check-sso.html', to: './' }]
     })
   ]
 };
