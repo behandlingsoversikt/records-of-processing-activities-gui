f8391f755073470d6d7804047363e8acce13f7d7
Lars M Riiser Haraldsen <39513382+lillelars@users.noreply.github.com>
2020-06-22 08:00:54 +0200

    changed article 6 hlptext

--- src/l10n/helptexts.nb.ts
+++ src/l10n/helptexts.nb.ts
@@ -19,10 +19,8 @@ export const helptextsNb = {
   purposeDescription: `Alle formål skal være forklart på en måte som gjør at alle berørte har samme forståelse av hva personopplysningene skal brukes til. At formålet skal være legitimt innebærer at det i tillegg til å ha et rettslig grunnlag også skal være i samsvar med øvrige etiske og rettslige samfunnsnormer. Personopplysninger kan ikke gjenbrukes til formål som er uforenelig med det opprinnelige formålet.
 
 Les mer om personvernprinsippene på [Datatilsynets nettsider](https://datatilsynet.no)`,
-  articleSixBasisAbstract: `Hvis behandlingen gjelder en rettslig forpliktelse, berettighet interesse mv. (c, e, eller f) kan du også henvise til annen lovgivning, dersom relevant.`,
-  articleSixBasisDescription: `All behandling av personopplysninger må ha et rettslig grunnlag for å være lov, jfr personvernforordningen artikkel 6. 
-
-Det må finnes et behandlingsgrunnlag for behandling av hver enkelt personopplysning til hvert enkelt formål. Dersom flere behandlingsgrunnlag kan passe, må virksomheten bestemme seg for ett grunnlag per formål.  `,
+  articleSixBasisAbstract: `All behandling av personopplysninger må ha et rettslig grunnlag, og er bare lovlig dersom minst ett av vilkårene i artikkel 6 nr. 1, bokstav a-f er oppfylt.`,
+  articleSixBasisDescription: `Det må finnes et behandlingsgrunnlag for behandling av hver enkelt personopplysning til hvert enkelt formål. Hvis behandlingen gjelder en rettslig forpliktelse, berettiget interesse med videre. (c, e, eller f) skal du også henvise til annen lovgivning.`,
   otherArticlesAbstract: `Det må foreligge et særskilt grunnlag i tillegg til behandlingsgrunnlag for å behandle denne typen opplysninger. Skriv inn henvisning også til annen lovgivning, dersom relevant.  `,
   otherArticlesDescription:
     'Les mer på [Datatilsynets nettsider](https://www.datatilsynet.no/rettigheter-og-plikter/virksomhetenes-plikter/behandlingsgrunnlag/veileder-om-behandlingsgrunnlag/?id=10832).',
