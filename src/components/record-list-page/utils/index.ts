b12ca354a99623737ebb3550891fef5ca432a080
pooriamehregan <pooriamehregan@gmail.com>
2022-06-13 21:40:44 +0200

    fix: avoid reading undefind contact details

--- src/components/record-list-page/utils/index.ts
+++ src/components/record-list-page/utils/index.ts
@@ -4,24 +4,24 @@ const isValidDataControllerRepresentativeInEU = ({
   dataControllerRepresentativeInEU
 }: RepresentativesInterface) =>
   !!(
-    (dataControllerRepresentativeInEU.name &&
-      dataControllerRepresentativeInEU.address &&
-      dataControllerRepresentativeInEU.email &&
-      dataControllerRepresentativeInEU.phone) ||
-    (!dataControllerRepresentativeInEU.name &&
-      !dataControllerRepresentativeInEU.address &&
-      !dataControllerRepresentativeInEU.email &&
-      !dataControllerRepresentativeInEU.phone)
+    (dataControllerRepresentativeInEU?.name &&
+      dataControllerRepresentativeInEU?.address &&
+      dataControllerRepresentativeInEU?.email &&
+      dataControllerRepresentativeInEU?.phone) ||
+    (!dataControllerRepresentativeInEU?.name &&
+      !dataControllerRepresentativeInEU?.address &&
+      !dataControllerRepresentativeInEU?.email &&
+      !dataControllerRepresentativeInEU?.phone)
   );
 
 const isValidDataProtectionOfficer = ({
   dataProtectionOfficer
 }: RepresentativesInterface) =>
   !!(
-    dataProtectionOfficer.name &&
-    dataProtectionOfficer.address &&
-    dataProtectionOfficer.email &&
-    dataProtectionOfficer.phone
+    dataProtectionOfficer?.name &&
+    dataProtectionOfficer?.address &&
+    dataProtectionOfficer?.email &&
+    dataProtectionOfficer?.phone
   );
 
 export const isValidRepresentatives = (
